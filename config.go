6ba68c296a44902653ccb2262bb20c496d3cb8f1
Midona <hauxhaux@gmail.com>
2019-12-18 21:41:07 +0100

    Revert "Revert "added throttle functionality""
    This reverts commit 48dab9e81ebde6e1d0f4b4322b72dd0ab077eaf4.

--- config.go
+++ config.go
@@ -30,34 +30,14 @@ type Config struct {
 		DefaultGuild string `json:"defaultGuild"`
 		Playing      string `json:"playing"`
 	} `json:"discord"`
-	Role struct {
-		Casters     []string `json:"Casters"`
-		RangedDPS   []string `json:"Ranged DPS"`
-		MeleeDPS    []string `json:"Melee DPS"`
-		Healers     []string `json:"Healers"`
-		Tanks       []string `json:"Tanks"`
-		Guest       []string `json:"Guest"`
-		Chaos       []string `json:"Chaos"`
-		Light       []string `json:"Light"`
-		RaidLeaders []string `json:"Raid Leaders"`
+	Role []struct {
+		RoleID string   `json:"roleID"`
+		Alias  []string `json:"alias"`
 	} `json:"role"`
-	Throttle struct {
-		StaticLfmLight struct {
-			MaxTokens     int `json:"maxTokens"`
-			TokenInterval int `json:"tokenInterval"`
-		} `json:"static_lfm_light"`
-		PlayerLfgLight struct {
-			MaxTokens     int `json:"maxTokens"`
-			TokenInterval int `json:"tokenInterval"`
-		} `json:"player_lfg_light"`
-		StaticLfmChaos struct {
-			MaxTokens     int `json:"maxTokens"`
-			TokenInterval int `json:"tokenInterval"`
-		} `json:"static_lfm_chaos"`
-		PlayerLfgChaos struct {
-			MaxTokens     int `json:"maxTokens"`
-			TokenInterval int `json:"tokenInterval"`
-		} `json:"player_lfg_chaos"`
+	Throttle []struct {
+		ChannelID     string `json:"channelID"`
+		MaxTokens     int    `json:"maxTokens"`
+		TokenInterval int    `json:"tokenInterval"`
 	} `json:"throttle"`
 	Monitor struct {
 		Output string   `json:"output"`
